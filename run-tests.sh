bf028e956e9647cebb72888a34367e9f7cb9f0db
JenkinsTest12 <33447974+JenkinsTest12@users.noreply.github.com>
2017-12-04 11:58:02 +0200

    added tests script

---
+++ run-tests.sh
@@ -0,0 +1,6 @@
+PATH=$WORKSPACE/venv/bin:/usr/local/bin:$PATH
+if [ ! -d "venv" ]; then
+        virtualenv venv
+fi
+. venv/bin/activate
+pip install -r requirements.txt --download-cache=/tmp/$JOB_NAME
